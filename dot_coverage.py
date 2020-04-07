052bec1413203a25679e869cc4bad5763b57ab89
AlyssaBlack <alyssablackburn@uchicago.edu>
2020-04-07 00:47:01 -0500

    added alignment bar graph, new sorted segments

--- dot_coverage.py
+++ dot_coverage.py
@@ -7,10 +7,9 @@ import base64
 from io import BytesIO
 import pandas as pd
 import itertools
-#from xhtml2pdf import pisa
  
-
-width_of_bars=500
+bin_size = 300
+width_of_bars=300
 bar_color = "#3498DB"
 line_palette = ['#FFEB3B', "#4CAF50", "#1B5E20"]
 x_labels = "SARS-COV2 RefSeq Assembly"
@@ -38,12 +37,11 @@ def fill_blanks(f_txt, max_length):
 	return filled
 
 
-def make_hist(data, max_length, bin_size):
+def make_hist(data, max_length):
 	'''Makes a histogram from position read counts
 
 	Inputs: data- np array of filled in posiiton read counts
 			max_length- int length of genome
-			bin_size- int size of bin
 	Outputs: bin_pos- list of locations for histogram bars on x axis
 			bin_counts- list of counts per bin
 	'''
@@ -55,25 +53,14 @@ def make_hist(data, max_length, bin_size):
 	return bin_pos, bin_counts
 
 
-def plot(f_paf, f_fa, max_length, bin_pos, bin_counts, write_file, new_tsv):
-	'''Plots coverage track on top of dot plot. Writes .png, .svg, and .tsv
+def make_new_tsv(f_paf, f_fa, new_tsv):
+	'''Merges .paf and .fa and writes new .tsv
 
 	Inputs: f_paf- string of .paf file for dot plot
 			f_fa- string of .fa file for dot plot
-			max_length- int length of genome
-			bin_pos- list of locations for histogram bars on x axis
-			bin_counts- list of counts per bin
-			write_file- string name of file to write plots to
-			new_tsv- string name of file to write from .paf and .fa
-
-	Output: fig- matplotlib fig of coverage track over dot plot
+			new_tsv- string name of file from merged .paf and .fa
+	Outputs: int length of .paf row
 	'''
-
-	fig, axs = plt.subplots(2, 1, sharex=True, sharey=False, figsize=(9,10), 
-		gridspec_kw={'height_ratios': [1, 9]})
-
-	offset = 0
-
 	f1 = open(f_fa)
 	f2 = open(f_paf)
 	f3 = open(new_tsv, 'w+')
@@ -87,7 +74,6 @@ def plot(f_paf, f_fa, max_length, bin_pos, bin_counts, write_file, new_tsv):
 			return sum(1 for _ in in_file)
 
 	last_line_number = row_count(f_paf)
-
 	row_paf = rd_paf.next()
 	for row_fa in rd_fa:
 		
@@ -95,39 +81,129 @@ def plot(f_paf, f_fa, max_length, bin_pos, bin_counts, write_file, new_tsv):
 			continue
 		
 		if row_fa[0][1:] == row_paf[0]:
-			col_fract = float(row_paf[9])/float(row_paf[10])
-			col_idx = int(math.ceil(col_fract*len(line_palette)))-1
-			col = line_palette[col_idx]
-			axs[1].plot([float(row_paf[7]), float(row_paf[8])], [float(row_paf[2])+offset, 
-				float(row_paf[3])+offset], linewidth=2, color=col)
-			offset += float(row_paf[1])
-
 			new_file.writerow(row_paf)
 			if last_line_number != rd_paf.line_num:
 				row_paf = rd_paf.next()
 			
 		else:
 			fa_offset = int(row_fa[3][4:])
-			offset += fa_offset
-			new_file.writerow([row_fa[0][1:], str(fa_offset)])
+			new_row = [row_fa[0][1:], str(fa_offset)]
+			new_file.writerow(new_row)
 
 	f1.close()
 	f2.close()
 	f3.close()
+	return len(row_paf)
 
-	axs[1].set_xlim((0, max_length))
-	axs[1].set_ylim((0, offset))
 
-	axs[1].set_xlabel(x_labels)
-	axs[1].set_ylabel(y_labels)
+def plot_alignment(ax_diagnostic, ax_align, f_csv):
+	'''
+	Plots horizontal bar graph of alignment percentages
 
-	axs[0].bar(bin_pos, bin_counts, width=width_of_bars, color=bar_color)
+	Inputs: ax_diagnostic- diagnostic symbol axis
+			ax_align- alignment bar graph axis
+			f_csv- string of .csv file for coronavirus alignment percentages
+	Outputs: ax_diagnostic- diagnostic symbol axis
+			ax_align- alignment bar graph axis
+	'''
+	align_data = pd.read_csv(f_csv)
+	ax_align.barh(align_data['label'], align_data['percentage'], color="#D2B4DE")
+	ax_align.set_xlim(0,100)
+	ax_align.tick_params(axis="y",direction="in", left="true", pad=-5)
+	plt.setp(ax_align.get_yticklabels(), ha="left")
 
-	axs[0].yaxis.set_major_locator(plt.MaxNLocator(3))
-	axs[1].yaxis.set_major_locator(plt.MaxNLocator(4))
-	axs[1].xaxis.set_major_locator(plt.MaxNLocator(4))
-	fig.tight_layout()
+	covid_val = align_data[align_data['label']==
+		'wuhan_seafood_market_pneumonia_virus_isolate_Wuhan_Hu_1']['percentage'].values[0]
 	
+	ax_diagnostic.plot([.425, .575], [.5, .5], linewidth=20, color='black')
+	if covid_val >= 75:
+		ax_diagnostic.plot([.5, .5], [.1, .9], linewidth=20, color='black')
+		ax_diagnostic.set_title("Positive",)
+	else:
+		ax_diagnostic.set_title("Negative")
+	
+	ax_diagnostic.set_xlim(0,1)
+	ax_diagnostic.set_ylim(0,1)
+	ax_diagnostic.axis('off')
+	
+	return ax_diagnostic, ax_align
+
+
+def plot_dot_plot(ax_coverage, ax_dot, new_tsv, max_length, bin_pos, bin_counts, len_row):
+	'''Create dot plot and coverage track.
+
+	Inputs: ax_coverage- coverage track axis
+			ax_dot- dot plot axis
+			new_tsv- string name of file from merged .paf and .fa
+			max_length- int length of genome
+			bin_pos- list of locations for histogram bars on x axis
+			bin_counts- list of counts per bin
+			len_row- int length of .paf rows
+	Outputs: ax_coverage- coverage track axis
+			ax_dot- dot plot axis
+	'''
+	offset = 0
+	col_names = [str(i) for i in range(len_row)]
+	merged = pd.read_csv(new_tsv, names=col_names, delimiter='	')
+	
+	merged_sort = merged.sort_values('7')
+	merged_sort['cum_offset'] = merged_sort['1'].cumsum()
+	merged_sort['upto_offset'] = merged_sort['cum_offset'].values - merged_sort['1'].values
+
+	plotted = merged_sort[merged_sort['2'].notnull()]
+	not_plotted = merged_sort[merged_sort['2'].isnull()]
+	
+	def dot_plot(x1, x2, y1, y2, segment_offset, num, denom):
+		col_fract = float(num)/float(denom)
+		col_idx = int(math.ceil(col_fract*len(line_palette)))-1
+		col = line_palette[col_idx]
+		ax_dot.plot([x1, x2], [y1+segment_offset, y2+segment_offset], linewidth=2, color=col)
+
+	plotted.apply(lambda row : dot_plot(row['7'], row['8'],
+                     row['2'], row['3'], row['upto_offset'], row['9'], row['10']), axis = 1)
+
+	offset += np.sum(merged_sort['1'].values)
+	ax_coverage.set_xlim(0, max_length)
+	ax_dot.set_xlim((0, max_length))
+	ax_dot.set_ylim((0, offset))
+
+	ax_dot.set_xlabel(x_labels)
+	ax_dot.set_ylabel(y_labels)
+
+	ax_coverage.bar(bin_pos, bin_counts, width=width_of_bars, color=bar_color)
+
+	ax_coverage.yaxis.set_major_locator(plt.MaxNLocator(3))
+	ax_coverage.xaxis.set_major_locator(plt.MaxNLocator(4))
+	ax_dot.yaxis.set_major_locator(plt.MaxNLocator(4))
+	ax_dot.xaxis.set_major_locator(plt.MaxNLocator(4))
+
+	return ax_coverage, ax_dot
+
+
+def plot(f_paf, f_fa, f_csv, max_length, bin_pos, bin_counts, write_file, new_tsv):
+	'''Plots coverage track on top of dot plot. Writes .png, .svg, and .tsv
+
+	Inputs: f_paf- string of .paf file for dot plot
+			f_fa- string of .fa file for dot plot
+			f_csv- string of .csv file for alignment plot
+			max_length- int length of genome
+			bin_pos- list of locations for histogram bars on x axis
+			bin_counts- list of counts per bin
+			write_file- string name of file to write plots to
+			new_tsv- string name of file to write from .paf and .fa
+
+	Output: fig- matplotlib fig of coverage track, dot plot, alignment plot
+	'''
+
+	fig, axs = plt.subplots(2, 2, sharex=False, sharey=False, figsize=(14.35,10), 
+		gridspec_kw={'height_ratios': [1, 9], 'width_ratios':[9, 5.35]})
+
+	len_row = make_new_tsv(f_paf, f_fa, new_tsv)
+	axs[0][0], axs[1][0] = plot_dot_plot(axs[0][0], axs[1][0], new_tsv, max_length, 
+		bin_pos, bin_counts, len_row)
+	axs[0][1], axs[1][1] = plot_alignment(axs[0][1], axs[1][1], f_csv)
+
+	fig.tight_layout()	
 	plt.savefig(write_file+'.svg',
 				dpi=None, facecolor='w', edgecolor='w',
 		        orientation='portrait', papertype=None, format=None,
@@ -146,54 +222,56 @@ def plot(f_paf, f_fa, max_length, bin_pos, bin_counts, write_file, new_tsv):
 	return fig
 
 
-def append_html(fig, html_file):
+def append_html(fig, f_html):
 	'''Opens html file, appends plots.
 
 	Inputs: fig- matplotlib fig of coverage track over dot plot
-			html_file- string name of html file to append plot to
+			f_html- string name of html file to append plot to
 	'''
 	tmpfile = BytesIO()
 	fig.savefig(tmpfile, format='png')
 	encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
 	html = '<img src=\'data:image/png;base64,{}\'>'.format(encoded)
 
-	with open(html_file,'a') as f:
+	with open(f_html,'a') as f:
 		f.write(html)
 
 
-def html_to_pdf(html_file, pdf_file):
+def html_to_pdf(f_html, new_pdf):
 	'''Converts html to pdf.
 
-	Inputs: html_file- string name of html file to append plot to
-			pdf_file- strang name of pdf file to convert html to
+	Inputs: f_html- string name of html file to append plot to
+			new_pdf- strang name of pdf file to convert html to
 	'''
 	import pdfkit
-	pdfkit.from_file(html_file, pdf_file)
+	pdfkit.from_file(f_html, new_pdf)
 	
 
-def main(f_txt, f_paf, f_fa, max_length, bin_size, write_file, html_file, 
-	pdf_file, convert_to_pdf, new_tsv):
+def main(f_txt, f_paf, f_fa, f_csv, f_html, max_length, write_file, new_pdf, 
+	new_tsv, convert_to_pdf,):
 	'''Reads .txt + .paf +.fa file, plots coverage track + dot plot, writes plots to file
 	Opens html file, appends plots, converts to pdf if convert_to_pdf is True
 
 	Inputs: f_txt- .txt file string to read for histogram
 			f_paf- .paf file string to read for dot plot
 			f_fa- .fa file string to read for dot plot
+			f_csv- for alignment bar graph
+			f_html- string name of html file to append plot to
 			max_length- int length of viral genome
-			bin_size- int bin size to use for histogram
 			write_file- string name of file to write plots to
-			html_file- string name of html file to append plot to
-			pdf_file- string name of pdf file to convert html to
+			new_pdf- string name of pdf file to convert html to
 			new_tsv- string name of new tsv file from .paf and .fa
+			convert_to_pdf- string, converts to pdf if == "convert"
 	'''
 	filled = fill_blanks(f_txt, max_length)
-	bin_pos, bin_counts = make_hist(filled, max_length, bin_size)
-	fig = plot(f_paf, f_fa, max_length, bin_pos, bin_counts, write_file, new_tsv)
+	bin_pos, bin_counts = make_hist(filled, max_length)
+	make_new_tsv(f_paf, f_fa, new_tsv)
+	fig = plot(f_paf, f_fa, f_csv, max_length, bin_pos, bin_counts, write_file, new_tsv)
 	
-	append_html(fig, html_file)
+	append_html(fig, f_html)
 
 	if convert_to_pdf == "convert":
-		html_to_pdf(html_file, pdf_file)
+		html_to_pdf(f_html, new_pdf)
 
 
 
@@ -202,7 +280,8 @@ if __name__ == "__main__":
 	Example usage:
 	python dot_coverage.py ".txt file" ".paf file" ".fa file" "destination for plots" "bin size" 
 		"genome length" "html stats" "new pdf" "pdf conversion?" "new tsv"
-	python dot_coverage.py depth_per_base.txt contig_nCoV-2019.paf final.contigs.fa covid_plots 500 29903 stats1.html stats1.pdf False new.tsv
+	python dot_coverage.py depth_per_base.txt contig_nCoV-2019.paf final.contigs.fa stats.csv stats.html 
+		29903 covid_plots stats.pdf new.tsv False 
 	'''
 	parser = argparse.ArgumentParser(description='Plot histogram.')
 	parser.add_argument('read_txt_file', metavar='rf_txt', type=str,
@@ -211,28 +290,28 @@ if __name__ == "__main__":
                      help='.paf file to read')
 	parser.add_argument('read_fa_file', metavar='rf_fa', type=str,
                      help='.fa file to read')
-	
-	parser.add_argument('write_file', metavar='wf', type=str,
-                     help='png/svg files to write')
-	parser.add_argument('bin_size', metavar='b', type=int,
-                     help='Histogram bin size')
+	parser.add_argument('read_csv_file', metavar='rf_csv', type=str,
+                     help='.csv file to read')
+	parser.add_argument('read_html_file', metavar='rf_html', type=str,
+                     help='.html file to add plot to')
+
 	parser.add_argument('max_length', metavar='m', type=int, 
                      help='Genome length')
 
-	parser.add_argument('read_html_file', metavar='rf_html', type=str,
-                     help='.html file to add plot to')
+	parser.add_argument('write_file', metavar='wf', type=str,
+                     help='png/svg files to write')
 	parser.add_argument('write_pdf_file', metavar='wf_pdf', type=str,
                      help='.pdf file to write html to')
+	parser.add_argument('new_tsv', metavar='wf_tsv', type=str, 
+                     help='new tsv from .paf and .fa')
 
 	parser.add_argument('convert_to_pdf', metavar='pdf_bool', type=str, 
                      help='do we convert to pdf')
 
-	parser.add_argument('new_tsv', metavar='wf_tsv', type=str, 
-                     help='new tsv from .paf and .fa')
-    
+	
 	args = parser.parse_args()
-	main(args.read_txt_file, args.read_paf_file, args.read_fa_file, 
-		args.max_length, args.bin_size, args.write_file, args.read_html_file, 
-		args.write_pdf_file, args.convert_to_pdf, args.new_tsv)
+	main(args.read_txt_file, args.read_paf_file, args.read_fa_file, args.read_csv_file, 
+		args.read_html_file, args.max_length, args.write_file, args.write_pdf_file, 
+		args.new_tsv, args.convert_to_pdf)
 	
 
