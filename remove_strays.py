8684b077f29d43e2b1cea841b08ae453265cb678
AlyssaBlack <alyssablackburn@uchicago.edu>
2020-04-25 12:04:35 -0500

    Update remove_strays.py
    handle very low noise

--- remove_strays.py
+++ remove_strays.py
@@ -8,11 +8,11 @@ threshold = 0
 def remove(cov_pd, number_list, wf):
 	new_list = np.zeros(number_list.shape)
 	zero_idx = np.where(number_list <= threshold)[0]
-	if zero_idx.shape[0] == 0:
-		#print('did nothing')
-		np.savetxt(wf, cov_pd.values, fmt='%s', delimiter='	')
-		return
-	#print(zero_idx)
+
+	zero_idx = np.insert(zero_idx, 0, -1)
+	zero_idx = np.insert(zero_idx, zero_idx.shape[0], len(number_list)-1)
+
+	
 	hole_length = np.ediff1d(zero_idx)
 	hole_loc = np.where(hole_length > length)[0]
 
@@ -22,7 +22,6 @@ def remove(cov_pd, number_list, wf):
 	for i in range(data_start.shape[0]):
 		new_list[data_start[i]:data_end[i]] = number_list[data_start[i]:data_end[i]]
 
-	#print(np.count_nonzero(new_list))
 	cov_pd['third'] = new_list
 	cov_pd['third'] = cov_pd['third'].astype(int)
 	np.savetxt(wf, cov_pd.values, fmt='%s', delimiter='	')
@@ -42,4 +41,5 @@ if __name__ == "__main__":
 	cov_pd = pd.read_csv(args.read_txt_file, sep="	", names=['first', 'second', 'third'], header=None)
 	number_list = cov_pd['third'].values
 	
+	#number_list = np.asarray([1, 2, 3, 0, 4, 5, 6])
 	remove(cov_pd, number_list, args.write_txt_file)
