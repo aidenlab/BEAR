eb98955883c2a8da20885c385e3ec6fc2e350efd
Neva C. Durand <nchernia@gmail.com>
2020-11-20 18:22:03 -0600

    updates

--- align_serial.sh
+++ align_serial.sh
@@ -13,7 +13,7 @@
 ### REQUIRED SOFTWARE 
 ## You must have the following software installed
 ## and available in your PATH
-## BWA; Samtools; Minimap2; Megahit; Python
+## BWA; Samtools
 
 ## Threads
 threads=16
@@ -22,9 +22,11 @@ VIRUS="SARS-CoV-2"
 ### VARIABLES: Automatically set
 TOP_DIR=$(pwd)
 PIPELINE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REF_DIR="${PIPELINE_DIR}/reference_files/*/*/*.fasta"
-MATCH_REF="${PIPELINE_DIR}/reference_files/target/*/*.fasta"
-MATCH_NAME=$(echo $MATCH_REF | sed 's:.*/::' | rev | cut -c7- | rev )
+REFERENCE="${PIPELINE_DIR}/reference_files/SARS-CoV_ISv0.4.1_seperate.fasta.gz"
+NT_TO_IS="${PIPELINE_DIR}/reference_files/"
+REMRECOMBO="${PIPELINE_DIR}/accuGenomics/remRecombo"
+COVERAGE="${PIPELINE_DIR}/accuGenomics/coverage"
+HUMAN_REF="${PIPELINE_DIR}/reference_files/chrM.fa.gz"
 
 # Usage and commands
 usageHelp="Usage: ${0##*/} [-d TOP_DIR] [-t THREADS] -jkrh"
@@ -85,7 +87,7 @@ else
         fi
     else
         echo "***! Failed to find any files matching ${FASTQ_DIR}"
-  printHelpAndExit 1
+	printHelpAndExit 1
     fi
 fi
 
@@ -102,21 +104,14 @@ do
     read2files+=($name2$ext)
 done
 
-# replace spaces with commas for megahit
-read1filescomma=$(echo "${read1files[*]}" | sed 's/ /,/g;s/,$//')
-read2filescomma=$(echo "${read2files[*]}" | sed 's/ /,/g;s/,$//')
-
-REFERENCES=$REF_DIR
 export WORK_DIR=${TOP_DIR}/polar-bear-fda-eua
 
 Align_Reference ()
 {
-    REFERENCE=$1
-    
     ######################################################################
     ########## Align 
     ######################################################################
-    REFERENCE_NAME=$(echo $REFERENCE | sed 's:.*/::' | rev | cut -c7- | rev )
+    REFERENCE_NAME=$(echo $1 | sed 's:.*/::' | rev | cut -c7- | rev )
 
     echo -e "ʕ·ᴥ·ʔ : Aligning files matching $FASTQ_DIR\n to $VIRUS reference assembly and Accukit control sequence"
 
@@ -125,14 +120,14 @@ Align_Reference ()
     if ! mkdir "${WORK_DIR}/${REFERENCE_NAME}/debug"; then echo "***! Unable to create ${WORK_DIR}/${REFERENCE_NAME}/debug! Exiting"; exit 1; fi
     
     for ((i = 0; i < ${#read1files[@]}; ++i)); do
-            file1=${read1files[$i]}
-            file2=${read2files[$i]}
+        file1=${read1files[$i]}
+        file2=${read2files[$i]}
         
         FILE=$(basename ${file1%$read1str})
         ALIGNED_FILE=${WORK_DIR}/${REFERENCE_NAME}/aligned/${FILE}"_mapped"
         
-            # Align reads
-        bwa mem -k 32 -t $threads $REFERENCE $file1 $file2 > $ALIGNED_FILE"_full_dataset.sam" 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/align.out
+        # Align reads
+        bwa mem -k 32 -t $threads $1 $file1 $file2 > $ALIGNED_FILE".sam" 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/align.out
         # Samtools fixmate and sort, output as BAM
         samtools fixmate -m $ALIGNED_FILE".sam" $ALIGNED_FILE".bam"
         samtools sort -@ $threads -o $ALIGNED_FILE"_matefixd_sorted.bam" $ALIGNED_FILE".bam"  2> ${WORK_DIR}/${REFERENCE_NAME}/debug/sort.out
@@ -146,62 +141,6 @@ Align_Reference ()
         rm ${WORK_DIR}/${REFERENCE_NAME}/aligned/*"_mapped"*bam  
     fi
 
-#echo "Removing Rcecombinants..."
-#	"$sdir/remRecombo" "$nt2is" "${pi}/${fn}.bam" $splitSeq "$sdir"
-#samtools index "${pi}/${fn}.bam"
-#rm -f "${pi}/${fn}-rsort.temp"
-
-#echo "Coverage..."
-#"$sdir/coverage" "${pi}/${fn}" "$coverage" "$covBed"
-
-#echo "CC QC..."
-#awk -v output="$runQC" -v cc_expected=$cc_expected -v is_input=$is_input '{if($2 == 1) {
-#printf "%s\t%s\t%i\t%i\t%i\t%i", $1, "1",$4,$5,$6,$3 >> output
-#                if($5+$4==0){printf "\t%s\n", "ND" >> output} else {printf "\t%f\n",($5/($5+$4)*$3)/cc_expected >> output}
-#} else if($2 == 2) {
-#printf "%s\t%s\t%i\t%i\t%i\t%i", $1, "2",$7,$8,$9,$3 >> output
-#                if($7+$8==0){printf "\t%s\n", "ND" >> output} else {printf "\t%f\n",($8/($8+$7)*$3)/cc_expected >> output}
-#  #old viral load calc if($5==0){printf "\t%s\n", "ND" >> output} else {printf "\t%i\n" , $4/$5*is_input >> output}
-#}}' "${pi}/${fn}.bam_CC-read-counts.txt"
-#
-#echo "Calculating viral load..."
-#awk -v is_input=$is_input -v vl="$viralLoad" 'FNR==1 {next}
-#{
-#if ($3+$4==0) {
-#                vlr="ND"
-#        } else {
-#                vlr=( $2 + ($4 / 2) )/($3 + ($4 / 2) ) * is_input
-#        }
-#        printf "%s\t%i\t%i\t%i\t%i\t%.1f\n", $1, $2 , $3 , $4 , is_input , vlr >> vl
-#}' "$coverage"
-#
-#
-
-
-    
-    if samtools markdup ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged.bam ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/dedup.out
-    then
-        rm ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged.bam
-    fi
-
-    samtools depth -a -Q 1 ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam | grep -v  "^nCov-control" > ${WORK_DIR}/${REFERENCE_NAME}/aligned/depth_per_base.txt 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/depth.out
-
-    samtools coverage -q 1 ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam > ${WORK_DIR}/${REFERENCE_NAME}/aligned/coverage_1.tsv 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/coverage_1.out
-    samtools coverage -q 0 ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam > ${WORK_DIR}/${REFERENCE_NAME}/aligned/coverage_0.tsv 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/coverage_0.out
-    
-        # In case you want to visualize the bams, index them. 
-    if [ -n "$produceIndex" ]
-    then
-        samtools index ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/index.out
-    fi
-
-        # Statistics 
-    echo "ʕ·ᴥ·ʔ :samtools flagstat result" > ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
-    samtools flagstat ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam  >> ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
-
-    echo "ʕ·ᴥ·ʔ : samtools stats result " >> ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
-    samtools stats ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam >> ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
-
 }
 
 if [[ "$afteralignment" -ne 1 ]]
@@ -209,16 +148,80 @@ then
     if ! mkdir "${WORK_DIR}"; then echo "***! Unable to create ${WORK_DIR}! Exiting"; exit 1; fi
     
     export -f Align_Reference
-    for REFERENCE in $REFERENCES        
+    for REF in $REFERENCE $HUMAN_REF
     do
-        Align_Reference $REFERENCE &
+        Align_Reference $REF &
     done
     wait
 fi
 echo "ʕ·ᴥ·ʔ : Done with alignment" 
 
+echo "ʕ·ᴥ·ʔ : Removing Recombinants..."
+
+REFERENCE_NAME=$(echo $1 | sed 's:.*/::' | rev | cut -c7- | rev )
+"${REMRECOMBO}" "${NT_TO_IS}" ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged.bam 0 
+
+#samtools index "${pi}/${fn}.bam"
+#rm -f "${pi}/${fn}-rsort.temp"
+
+echo "ʕ·ᴥ·ʔ : Calculating Viral Load..."
+
+#average reads per amplicon = total bases / expected fragment length 
+# print coverage, median value
+#echo -e "SAMPLE\tNT_READS\tIS_READS\tREC_READS\tNT_AVG\tIS_AVG\tREC_AVG" > "$coverage"
+samtools bedcov -Q 4 "$NT_TO_IS" "${stem}-good.bam" | awk '$1=="MN908947.3" { ar=int($9/($3-$2)); nt+=ar}END{printf ("%i\t",  nt)}' > ${WORK_DIR}/aligned/ampliconCoverage.txt 
+samtools bedcov -Q 4 "$NT_TO_IS" "${stem}-IS.bam" | awk '$1 ~ /-SNAQ$/) { ar=int($9/($3-$2)); nt+=ar }END{printf ("%i\t",  nt)}' >> ${WORK_DIR}/aligned/ampliconCoverage.txt 
+samtools bedcov -Q 4 "$NT_TO_IS" "${stem}-bad.bam" | awk '{ ar=int($9/($3-$2)); nt+=ar}END{printf ("%i\n",  nt)}' >> ${WORK_DIR}/aligned/ampliconCoverage.txt 
+
+awk -v threshold=$viralThreshold -v controlThreshold=$controlThreshold -v humanThreshold=$humanThreshold '{
+ recomb = $3/2; 
+ viral_load = 100*($1 + recomb)/($2 + recomb);
+ if (viral_load > threshold) { 
+   print "Positive";
+ }
+ else {
+   if (100*$1 > controlThreshold) {
+     if (humanThreshold) {
+       print "Negative";
+     }
+     else {
+       print "Undetermined, no human detected";
+     }
+   }
+   else {
+     print "Undetermined, no control detected";
+   }
+}'  ${WORK_DIR}/aligned/ampliconCoverage.txt
+
+
+## NEVA HERE PAUSED
+##
 # Gather alignment statistics
 echo "ʕ·ᴥ·ʔ : Compiling results" 
+    
+if samtools markdup ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged.bam ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/dedup.out
+then
+    rm ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged.bam
+fi
+
+samtools depth -a -Q 1 ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam | awk '$1=="MN908947.3"' > ${WORK_DIR}/${REFERENCE_NAME}/aligned/depth_per_base.txt 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/depth.out
+
+#    samtools coverage -q 1 ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam > ${WORK_DIR}/${REFERENCE_NAME}/aligned/coverage_1.tsv 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/coverage_1.out
+#    samtools coverage -q 0 ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam > ${WORK_DIR}/${REFERENCE_NAME}/aligned/coverage_0.tsv 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/coverage_0.out
+
+# In case you want to visualize the bams, index them. 
+if [ -n "$produceIndex" ]
+then
+    samtools index ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam 2> ${WORK_DIR}/${REFERENCE_NAME}/debug/index.out
+fi
+
+# Statistics 
+echo "ʕ·ᴥ·ʔ :samtools flagstat result" > ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
+samtools flagstat ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam  >> ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
+
+echo "ʕ·ᴥ·ʔ : samtools stats result " >> ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
+samtools stats ${WORK_DIR}/${REFERENCE_NAME}/aligned/sorted_merged_dups_marked.bam >> ${WORK_DIR}/${REFERENCE_NAME}/aligned/alignment_stats.txt
+
 
 REFERENCE_NAME='Wuhan_seafood_market_pneumonia_virus_isolate_Wuhan_Hu_1'
 
