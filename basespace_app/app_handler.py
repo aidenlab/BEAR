70475f99c6cb5e8d227a5cb8425af3625de51cc3
peradastra <briansaint@me.com>
2021-12-28 21:10:12 -0600

    Update app_handler.py

--- basespace_app/app_handler.py
+++ basespace_app/app_handler.py
@@ -1,6 +1,7 @@
 import os
-import subprocess
+import fnmatch
 import json
+
 
 # load json file
 ## define JSON format of the "AppSession.json" for the app session
@@ -23,64 +24,33 @@ def metadatajson():
     )
     return json_file
 
-def load_appsession_json(jsonPath='/data/input/AppSession.json'):
-    # open and load "AppSession.json" for the app session
-    jsonfile = open(jsonPath)
-    jsonAppObject = json.load(jsonfile)
-    return jsonAppObject
+## open and load "AppSession.json" for the app session
+jsonfile = open('data/input/AppSession.json')
+jsonObject = json.load(jsonfile)
 
-def get_app_data_from_json(jsonObject):
-    # get info necessary to
-    # determine the number of properties
-    numberOfPropertyItems = len(jsonObject['Properties']['Items'])
+# get info necessary to
+# determine the number of properties
+numberOfPropertyItems = len(jsonObject['Properties']['Items'])
 
-    # get "projectID" and "sampleID" information
-    projectID = []
-    sampleID = []
-    for index in range(numberOfPropertyItems):
-        if jsonObject['Properties']['Items'][index]['Name'] == 'Input.Projects':
-            projectID = jsonObject['Properties']['Items'][index]['Items'][0]['Id']
-        if jsonObject['Properties']['Items'][index]['Name'] == 'Input.Samples':
-            for sample in range(len(jsonObject['Properties']['Items'][index]['Items'])):
-                sampleID.append(jsonObject['Properties']['Items'][index]['Items'][sample]['Id'])
+sampleID = []
+projectID = []
+R1files = []
+R2files = []
+for index in range(numberOfPropertyItems):
+    if jsonObject['Properties']['Items'][index]['Name'] == 'Input.Projects':
+        projectID = jsonObject['Properties']['Items'][index]['Items'][0]['Id']
+    if jsonObject['Properties']['Items'][index]['Name'] == 'Input.Samples':
+        for sample in range(len(jsonObject['Properties']['Items'][index]['Items'])):
+            sampleID.append(jsonObject['Properties']['Items'][index]['Items'][sample]['Id'])
+            sampleDir = '/data/input/samples/%s' % (sampleID[sample])
+            for root, dirs, files in os.walk(sampleDir[sample]):
+                R1files.append(fnmatch.filter(files, '*_R1_*'))
+                R2files.append(fnmatch.filter(files, '*_R2_*'))
 
-    # set sample directory containing FASTQs
-    sampleDir = '/data/input/samples/%s' % (sampleID[sample])
+sampleOutDir = '/data/output/appresults/%s/%s' %(projectID,"output_here")
+os.system('mkdir -p "%s"' %(sampleOutDir))
 
-    # set output directory
-    bearOutDir = '/data/output/appresults/%s' % (projectID)
-
-    return sampleDir, projectID, bearOutDir
-
-
-def create_file_trigger_for_bear_pipeline(bearOutDir):
-    # create "parameters.csv" file in output folder for BEAR
-    parameterFile = open("POLAR-BEAR/native.app.txt" ,'w')
-    parameterFile.write(bearOutDir)
-    parameterFile.close()
-
-def create_file(bearOutDir):
-    # create "parameters.csv" file in output folder for BEAR
-    file_name = bearOutDir + '/test.txt'
-    parameterFile = open(file_name ,'w')
-    parameterFile.write(bearOutDir)
-    parameterFile.close()
-
-def run_polar_bear():
-    # create & open log file for bear pipeline
-    bear_pipeline_log = open('data/logs/polar_bear_eua_pipline_log.txt', 'w')
-
-    # run bear command
-    bear_command = ["bash", "POLAR-BEAR/run_polar_bear_eua_pipline.sh", "-d", "POLAR-BEAR/test"]
-    bear_out = subprocess.call(bear_command, stdout=bear_pipeline_log)
-
-    # close log file for bear pipeline
-    bear_pipeline_log.close()
-
-
-if __name__ == "__main__":
-    jsonAppObject = load_appsession_json()
-    sampleDir, projectID, bearOutDir = get_app_data_from_json(jsonAppObject)
-    create_file_trigger_for_bear_pipeline(bearOutDir)
-    run_polar_bear()
-
+file = '%s/parameters.csv' %(sampleOutDir)
+outFile = open(file ,'w')
+outFile.write('This is a test.')
+outFile.close()
